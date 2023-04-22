95dd3af90640ebf84c69715e8a2b81d3a42b442f
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-04-22 12:26:48 -0300

    remove anv

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ImageModule } from './image/image.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://process.env.DB_MONGO:${process.env.DB_KEY}@cluster0.uboej.mongodb.net/?retryWrites=true&w=majority`,
+      'mongodb+srv://mongodbrd:ErfDh4b0slpeNtkk@cluster0.uboej.mongodb.net/?retryWrites=true&w=majority',
     ),
     ImageModule,
   ],
